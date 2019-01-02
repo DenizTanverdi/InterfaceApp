a45f9584e3885efae2e8657bbd93126d08302525
semvs <semvs@ayvansaray.edu.tr>
2019-01-02 09:10:52 +0300

    interface

---
+++ InterfaceApp/Kisi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApp
+{
+    class Kisi : Kimlik
+    {
+        public cinsiyet Cinsiyet { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public din Din { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string TcKimlikNo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DogumYeri { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DogumTarihi { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Ad { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Soyad { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string BabaAdi { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string FullName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
